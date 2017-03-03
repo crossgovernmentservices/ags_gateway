6b138a2b976e43045872d79e0177e807ae6f5f64
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-03-03 15:59:40 +0000

    Refactored test in line with review

--- tests/functional/test_to_service_interstitial.py
+++ tests/functional/test_to_service_interstitial.py
@@ -1,5 +1,3 @@
-import re
-
 from bs4 import BeautifulSoup
 from flask import url_for
 import pytest
@@ -15,8 +13,7 @@ class When_on_to_service_interstitial(object):
 
     @pytest.fixture(autouse=True)
     def setup_page(self, client, app):
-        self.config_countdown = app.config['META_REFRESH_DELAY']
-
+        self.delay = app.config['META_REFRESH_DELAY']
         self.service_page_redirect = 'to-service-page'
 
         with client.session_transaction() as session:
@@ -24,16 +21,13 @@ class When_on_to_service_interstitial(object):
 
         self.response = request(url_for('main.to_service'), client.get)
 
-    def it_has_continue_link_href_set_to_service_page(
-            self, client):
-        next_li = self.response.soup.find("li", class_="next")
+    def it_has_continue_link_href_set_to_service_page(self, client):
+        next_link = self.response.soup.select_one('.next a')
 
-        assert self.service_page_redirect == next_li.find('a')['href']
+        assert self.service_page_redirect == next_link['href']
 
-    def it_has_countdown_set_to_the_meta_refresh_config(self, config):
-        countdown_text = self.response.soup.select_one(
-            "#content span.pagination-text span").text
+    def it_has_countdown_set_to_the_meta_refresh_config(self):
+        redirect_timer_text = self.response.soup.select_one(
+            ".redirect-timer").text
 
-        countdown_int = int(re.match(r'\d+', countdown_text).group())
-
-        assert countdown_int == self.config_countdown
+        assert "{} seconds".format(self.delay) in redirect_timer_text
